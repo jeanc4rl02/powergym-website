d5ad51f2398cd7d409263501f09ff312b86efc5a
Jean Carlos Carrillo <102492675+jeanc4rl02@users.noreply.github.com>
2024-12-13 00:13:32 -0500

    iconify and responsive added

--- Frontend/tailwind.config.mjs
+++ Frontend/tailwind.config.mjs
@@ -1,3 +1,5 @@
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
@@ -7,6 +9,7 @@ export default {
 		},
 	},
 	plugins: [
-		require('flowbite/plugin')
+		require('flowbite/plugin'),
+		addDynamicIconSelectors()
 	],
 }
